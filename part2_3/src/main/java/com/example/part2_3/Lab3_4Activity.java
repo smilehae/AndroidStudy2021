1bb15a9749e8229cf69f9c05a5d6b9a5f2534b86
smilehae <mihaepyun@gmail.com>
2021-01-06 10:52:10 +0900

    이미지뷰가 사이즈를 넘어갈땐 maxHeight 속성과 adjustViewBound를 동시에 설정해야 한다!

---
+++ part2_3/src/main/java/com/example/part2_3/Lab3_4Activity.java
@@ -0,0 +1,14 @@
+package com.example.part2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab3_4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3_4);
+    }
+}
