38b4dd7176c50264767bf90cc02a99f4f10a5803
smilehae <mihaepyun@gmail.com>
2021-01-06 11:19:04 +0900

    text, image, editText, checkBox 관련

--- part2_3/src/main/java/com/example/part2_3/Lab3_4Activity.java
+++ part2_3/src/main/java/com/example/part2_3/Lab3_4Activity.java
@@ -2,13 +2,39 @@ package com.example.part2_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class Lab3_4Activity extends AppCompatActivity {
+
+    TextView textView;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_4);
+
+        textView = findViewById(R.id.custom_font);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"xmas.ttf");
+        textView.setTypeface(typeface);
+
+        checkBox = findViewById(R.id.checker);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean ischecked) {
+                if(ischecked){
+                    checkBox.setText("is Checked");
+                }
+                else{
+                    checkBox.setText("is Unchecked");
+                }
+            }
+        });
     }
+
+
 }
