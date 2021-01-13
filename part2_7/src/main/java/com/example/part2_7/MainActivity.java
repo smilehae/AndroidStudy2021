ef3b4e8be4b4875686d819dd0ee02f35afafe344
smilehae <mihaepyun@gmail.com>
2021-01-14 01:27:06 +0900

    Part 2_7의 첫번째 실습 완료

---
+++ part2_7/src/main/java/com/example/part2_7/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.part2_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView.findViewById(R.id.img);
+        //애니메이션 획득
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.in);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation anim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.move);
+                //마지막에 멈추도록 설정
+                anim.setFillAfter(true);
+                imageView.startAnimation(anim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(anim);
+    }
+}
